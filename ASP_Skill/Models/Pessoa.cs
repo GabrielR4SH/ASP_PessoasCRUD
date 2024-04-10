fa10382522a74e394fbbcee2440ce8da1712697c
GabrielR4SH <bieelsantana23@hotmail.com>
2024-04-09 23:16:45 -0300

    Adicionar arquivos de projeto.

---
+++ ASP_Skill/Models/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP_Skill.Models
+{
+    [Table("Pessoa")]
+    public class Pessoa
+    {
+        // Atributos
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        public DateTime? DataNascimento { get; set; }
+
+        [StringLength(1)]
+        public string Sexo { get; set; }
+
+        [StringLength(10)]
+        public string EstadoCivil { get; set; }
+
+        [StringLength(11)]
+        public string CPF { get; set; }
+
+        [StringLength(100)]
+        public string Endereço { get; set; }
+
+        [StringLength(10)]
+        public string Numero { get; set; }
+
+        [StringLength(12)]
+        public string CEP { get; set; }
+
+        [StringLength(200)]
+        public string Complemento { get; set; }
+
+        [StringLength(30)]
+        public string Bairro { get; set; }
+
+        [StringLength(30)]
+        public string Cidade { get; set; }
+
+        [StringLength(2)]
+        public string UF { get; set; }
+    }
+}
